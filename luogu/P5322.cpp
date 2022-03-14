24011e667146e2db9f23c8ced7a2f249cca8a0f2
99_wood <2941426785@qq.com>
2022-03-15 00:10:20 +0800

    Add files via upload

---
+++ luogu/P5322.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<queue>
+#include<map>
+#include<set>
+#define rep(i,a,b) for(int i=a;i<=(b);++i)
+#define frep(i,a,b) for(int i=a;i<(b);++i)
+#define drep(i,a,b) for(int i=a;i>=(b);--i)
+#define mid ((l + r) >> 1)
+#define ls (p << 1)
+#define rs (p << 1 | 1)
+typedef long long ll;
+typedef unsigned long long ull;
+#define Maxq priority_queue<int,vector<int>,greater<int> >
+using namespace std;
+int mylog(int a) {
+	int ans=0;
+	if(a&0xffff0000) {
+		ans+=16;
+		a>>=16;
+	}
+	if(a&0x0000ff00) {
+		ans+=8;
+		a>>=8;
+	}
+	if(a&0x000000f0) {
+		ans+=4;
+		a>>=4;
+	}
+	if(a&0x0000000c) {
+		ans+=2;
+		a>>=2;
+	}
+	if(a&0x00000002) {
+		ans+=1;
+		a>>=1;
+	}
+	return ans;
+}
+inline int read() {
+	register int a=0,b=0;
+	register char c;
+	c=getchar();
+	while(c<'0'||c>'9') {
+		if(c=='-')b=1;
+		c=getchar();
+	}
+	while(c>='0'&&c<='9') {
+		a*=10;
+		a+=c-'0';
+		c=getchar();
+	}
+	return b?-a:a;
+}
+const int MAXN = 20010;
+int a[MAXN][200];
+ll f[MAXN];
+//int bs(int n, int p, int w){
+//	int l = 1, r = n + 1;
+//	while(l < r - 1){
+//		int m = (l + r) >> 1;
+//		if(a[p][m] * 2 < w) l = m;
+//		else r = m - 1;
+//	}
+//	return l;
+//}
+int main(){
+	int s = read(), n = read(), m = read();
+	rep(i, 1, s){
+		rep(j, 1, n){
+			a[j][i] = read();
+		}
+	}
+	rep(i, 1, n){
+		sort(a[i] + 1, a[i] + 1 + s);
+	}
+	rep(i, 1, n){
+		drep(j, m, 0){
+			rep(k, 0, s){
+				if(a[i][k] * 2 >= j) break;
+				f[j] = max(f[j], f[j - a[i][k] * 2 - 1] + k * i);
+			}
+		}
+	}
+	printf("%lld", f[m]);
+	return 0;
+}
