1019d20107cf780afd53c200f2c5601fe847ef69
99_wood <2941426785@qq.com>
2024-11-11 19:25:06 +0800

    update

---
+++ luogu/P1447.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<vector>
+#define endl '\n'
+#define int long long
+#define rep(i,a,b) for(int i=a;i<=(b);++i)
+#define frep(i,a,b) for(int i=a;i<(b);++i)
+#define drep(i,a,b) for(int i=a;i>=(b);--i)
+using namespace std;
+const int MAXN = 2000020;
+int mu[MAXN];
+bool flg[MAXN];
+vector<int> prime;
+void init(){
+	const int N = 2000000;
+	mu[1] = 1;
+	rep(i, 2, N){
+		if(!flg[i]){
+			prime.push_back(i);
+			mu[i] = -1;
+		}
+		for(int j = 0; j < prime.size() && prime[j] * i <= N; ++j){
+			int p = prime[j];
+			flg[p * i] = true;
+			if(i % p == 0){
+				mu[p * i] = 0;
+				break;
+			}
+			mu[i * p] = -mu[i];
+		}
+	}
+}
+void solve(){
+	int n, m;
+	cin >> n >> m;
+	int ans = 0;
+	rep(d, 1, min(n, m)){
+		rep(q, 1, min(n, m) / d){
+			ans += mu[q] * (n / d / q) * (m / d / q) * d;
+		}
+	}
+	ans *= 2;
+	ans -= n * m;
+	cout << ans;
+}
+signed main(){
+	init();
+//	rep(i, 1, 100) cout << mu[i] << " ";
+//    ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+    int _t=1;
+//    cin>>_t;
+    //    cout<<fixed<<setprecision(20);
+    for(int i=1;i<=_t;i++){
+        //cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;
+}
+
---
+++ luogu/P3455.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#define endl '\n'
+#define int long long
+#define rep(i,a,b) for(int i=a;i<=(b);++i)
+#define frep(i,a,b) for(int i=a;i<(b);++i)
+#define drep(i,a,b) for(int i=a;i>=(b);--i)
+using namespace std;
+const int MAXN = 2000020;
+int mu[MAXN];
+bool flg[MAXN];
+vector<int> prime;
+int sum[MAXN];
+void init(){
+	const int N = 2000000;
+	mu[1] = 1;
+	rep(i, 2, N){
+		if(!flg[i]){
+			prime.push_back(i);
+			mu[i] = -1;
+		}
+		for(int j = 0; j < prime.size() && prime[j] * i <= N; ++j){
+			int p = prime[j];
+			flg[p * i] = true;
+			if(i % p == 0){
+				mu[p * i] = 0;
+				break;
+			}
+			mu[i * p] = -mu[i];
+		}
+	}
+	rep(i, 1, N) sum[i] = sum[i - 1] + mu[i];
+	return;
+}
+void solve(){
+	int n, m, d;
+	cin >> n >> m >> d;
+	int a = n / d, b = m / d;
+	int ans = 0;
+	int l = 1, r = 1;
+	while(l <= min(a, b)){
+		r = min(a / (a / l), b / (b / l));
+		ans += (sum[r] - sum[l - 1]) * (a / l) * (b / l);
+		l = r + 1;
+	}
+//	rep(q, 1, min(a, b)){
+//		ans += mu[q] * (a / q) * (b / q);
+//	}
+	cout << ans << endl;
+}
+signed main(){
+	init();
+    ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+    int _t=1;
+    cin>>_t;
+    //    cout<<fixed<<setprecision(20);
+    for(int i=1;i<=_t;i++){
+        //cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;
+}
